86d6464a3f09b7dfc48fe8d6608092269629b80f
Dudda-soares <eduarda.soaresdsantos@gmail.com>
2025-05-22 22:48:35 -0300

    criação das seçoes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
-const imagemin = require('gulp-imagemin');
+const terser = require('gulp-terser');
 
 function style(){
     return gulp.src('./src/styles/*.scss')
@@ -8,11 +8,20 @@ function style(){
     .pipe(gulp.dest('./dist/css'))
 }
 
+function js(){
+    return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.bundle.js',
+      'src/scripts/**/*.js'])
+    .pipe(terser())
+    .pipe(gulp.dest('./dist/js'))
+}
 
 
-exports.default = style, image
+
+exports.default = gulp.parallel(style, js)
 exports.watch = function(){
-    gulp.watch('./src/styles/*.scss',style)
+    gulp.watch('./src/styles/*.scss',gulp.parallel(style))
+    gulp.watch('./src/scripts/**/*js',gulp.parallel(js))
+
     
 
 }
